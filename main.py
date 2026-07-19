2e868cffc524127c1a1f45eb1ebd6f1307fe8acf
Melvin <melvinlau88@outlook.com>
2026-07-19 12:06:18 +1000

    Extracts the audio track from video.mp4 and saves it as audio.wav, converted to the format (16kHz, mono)

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 import subprocess
 
+'''
+Extracts the audio track from video.mp4 and saves it as audio.wav,
+converted to the format (16kHz, mono)
+'''
+subprocess.run([
+    "ffmpeg",
+    "-y",
+    "-i", "video.mp4",     
+    "-vn",                 
+    "-ar", "16000",
+    "-ac", "1",         
+    "audio.wav"             
+])
+
